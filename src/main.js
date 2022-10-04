49b22d2c97ef3ee235a7be28ec7480d00c95b383
drawbu <69208565+drawbu@users.noreply.github.com>
2022-10-04 10:44:21 +0200

    :bug: Forget old import dependency.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 import './app.css'
 import App from './App.svelte'
-import feather from 'feather-icons'
 
 
-const app = new App({
-  target: document.body,
-})
-
-feather.replace();
+const app = new App({ target: document.body })
 
 export default app
