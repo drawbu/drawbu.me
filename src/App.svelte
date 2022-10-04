426354308c382bf0f61ae511623f7b5acc94f80a
drawbu <69208565+drawbu@users.noreply.github.com>
2022-10-04 10:42:58 +0200

    :truck: Move `<title>` declaration

--- src/App.svelte
+++ src/App.svelte
@@ -20,6 +20,7 @@
     href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap"
     rel="stylesheet"
   />
+  <title>drawbu.me</title>
 </head>
 
 <div class="w-auto md:w-[50rem] m-auto pr-4 pl-4" id="app">
