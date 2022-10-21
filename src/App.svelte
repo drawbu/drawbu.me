3a42d56c4695c0bd41d5611dc685d253254a17e7
drawbu <69208565+drawbu@users.noreply.github.com>
2022-10-21 14:56:20 +0200

    :zap: Move head from src/App.svelte to index.html

--- src/App.svelte
+++ src/App.svelte
@@ -13,16 +13,6 @@
   export let url = '';
 </script>
 
-<head>
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap"
-    rel="stylesheet"
-  />
-  <title>drawbu.me</title>
-</head>
-
 <div id="app">
   <Header />
   <main>
